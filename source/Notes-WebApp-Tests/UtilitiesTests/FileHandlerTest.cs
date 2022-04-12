478956532e7b665aa2c1430fc3d3be7302649114
Kasey E. Nieman <knknieman@gmail.com>
2022-04-12 15:20:43 -0500

    Finished FileHandler Tests

--- source/Notes-WebApp-Tests/UtilitiesTests/FileHandlerTest.cs
+++ source/Notes-WebApp-Tests/UtilitiesTests/FileHandlerTest.cs
@@ -4,23 +4,12 @@ using Notes_WebApp_Boomtown.Src.Notes;
 using Notes_WebApp_Boomtown.Src.Utilities;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Notes_WebApp_Tests
 {
     public class FileHandlerTest
     {
-
-        [SetUp]
-        public void Setup()
-        {
-
-        }
-
-        [TearDown]
-        public void Cleanup()
-        {
-
-        }
 
         [Test]
         public void WriteReadJsonToFile()
@@ -28,7 +17,6 @@ namespace Notes_WebApp_Tests
             Dictionary<string, string> testDict = new Dictionary<string, string> { ["KEY1"] = "VALUE1", ["KEY2"] = "VALUE2" };
             string json = FileHandler.GetJsonFromObject(testDict);
             FileHandler.WriteToFile("jsonSerializationTest.json", json);
-            //"\\TestData\\jsonSerializationTest.json"
             Dictionary<string, string> loadedDict = FileHandler.LoadJsonFile<Dictionary<string, string>>("jsonSerializationTest.json");
 
             foreach (KeyValuePair<string, string> entry in loadedDict)
@@ -36,6 +24,49 @@ namespace Notes_WebApp_Tests
                 Assert.AreEqual(testDict[entry.Key], entry.Value);
             }
 
+        }
+
+        [Test]
+        public void WriteReadFile()
+        {
+            string content = "TEST";
+            FileHandler.WriteToFile("testFile.txt", content);
+
+            string contentLoaded = FileHandler.GetFileContent("testFile.txt");
+            Assert.IsTrue(content.Equals(contentLoaded));
+        }
+
+        [Test]
+        public void ReadFileFail()
+        {
+            bool passed;
+            try
+            {
+                FileHandler.GetFileContent("FileDoesnotexist.txt");
+                passed = false;
+            }
+            catch(FileNotFoundException)
+            {
+                passed = true;
+            }
+            Assert.IsTrue(passed);
+        }
+
+
+        [Test]
+        public void ReadJsonFileFile()
+        {
+            bool passed;
+            try
+            {
+                FileHandler.LoadJsonFile<Dictionary<string, string>>("FileDoesnotexist.txt");
+                passed = false;
+            }
+            catch (FileNotFoundException)
+            {
+                passed = true;
+            }
+            Assert.IsTrue(passed);
         }
     }
 }
